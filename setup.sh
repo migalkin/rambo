1b8870e1ea665f5c29871eeedd64ee7ef4ffc87f
Priyansh Trivedi <pc.priyansh@gmail.com>
2019-08-16 11:02:28 +0200

    Trying out wandb

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@ git clone https://github.com/geraltofrivia/mytorch.git
 cd mytorch
 chmod +x setup.sh
 ./setup.sh
+
+
+mkdir data/parsed_data
+mkdir data/pre_training_data
+
+cd data/raw_data
+tar -xvf fb15k_wd.tar.gz
+
+cd ../..
