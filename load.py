7490320c36f422b989a7f6568f4f60642d49f8b2
Michael Galkin <mikhgalkin@gmail.com>
2019-08-30 16:17:26 +0200

    loader for wd15k

--- load.py
+++ load.py
@@ -5,40 +5,98 @@
 from utils import *
 
 
-def load_wd15k() -> (List, List, Dict[str, int], Dict[str, int], _):
+def load_wd15k_quints() -> Dict:
     """
 
     :return:
     """
 
     # Load data from disk
-    with open(PARSED_DATA_DIR / 'parsed_raw_data.pkl', 'rb') as f:
-        raw_data = pickle.load(f)
+    WD15K_DIR = PARSED_DATA_DIR / 'wd15k'
+    with open(WD15K_DIR/ 'train_quints.pkl', 'rb') as f:
+        train_quints = pickle.load(f)
+    with open(WD15K_DIR/ 'valid_quints.pkl', 'rb') as f:
+        valid_quints = pickle.load(f)
+    with open(WD15K_DIR/ 'test_quints.pkl', 'rb') as f:
+        test_quints = pickle.load(f)
 
-    # with open('./data/parsed_data/parsed_raw_data.pkl', 'rb') as f:
-    #     raw_data = pickle.load(f)
+    quints_entities, quints_predicates = [], []
 
-    entities, predicates = [], []
-
-    for quint in raw_data:
-        entities += [quint[0], quint[2]]
+    for quint in train_quints+valid_quints+test_quints:
+        quints_entities += [quint[0], quint[2]]
         if quint[4]:
-            entities.append(quint[4])
+            quints_entities.append(quint[4])
 
-        predicates.append(quint[1])
+        quints_predicates.append(quint[1])
         if quint[3]:
-            predicates.append(quint[3])
+            quints_predicates.append(quint[3])
 
-    entities = sorted(list(set(entities)))
-    predicates = sorted(list(set(predicates)))
+    quints_entities = sorted(list(set(quints_entities)))
+    quints_predicates = sorted(list(set(quints_predicates)))
 
-    entities = ['__na__', '__pad__'] + entities
-    predicates = ['__na__', '__pad__'] + predicates
+    q_entities = ['__na__'] + quints_entities
+    q_predicates = ['__na__'] + quints_predicates
 
     # uritoid = {ent: i for i, ent in enumerate(['__na__', '__pad__'] + entities +  predicates)}
-    entoid = {pred: i for i, pred in enumerate(entities)}
-    prtoid = {pred: i for i, pred in enumerate(predicates)}
+    entoid = {pred: i for i, pred in enumerate(q_entities)}
+    prtoid = {pred: i for i, pred in enumerate(q_predicates)}
+
+    train = [[entoid[q[0]],
+              prtoid[q[1]],
+              entoid[q[2]],
+              prtoid[q[3]] if q[3] is not None else prtoid['__na__'],
+              entoid[q[4]] if q[4] is not None else entoid['__na__']] for q in train_quints]
+    valid = [[entoid[q[0]],
+              prtoid[q[1]],
+              entoid[q[2]],
+              prtoid[q[3]] if q[3] is not None else prtoid['__na__'],
+              entoid[q[4]] if q[4] is not None else entoid['__na__']] for q in valid_quints]
+    test = [[entoid[q[0]],
+             prtoid[q[1]],
+             entoid[q[2]],
+             prtoid[q[3]] if q[3] is not None else prtoid['__na__'],
+             entoid[q[4]] if q[4] is not None else entoid['__na__']] for q in test_quints]
+
+    return {"train": train, "valid": valid, "test": test}
 
 
+def load_wd15k_triples() -> Dict:
+    """
+
+    :return:
+    """
+
+    # Load data from disk
+    WD15K_DIR = PARSED_DATA_DIR / 'wd15k'
+
+    with open(WD15K_DIR / 'train_triples.pkl', 'rb') as f:
+        train_triples = pickle.load(f)
+    with open(WD15K_DIR / 'valid_triples.pkl', 'rb') as f:
+        valid_triples = pickle.load(f)
+    with open(WD15K_DIR / 'test_triples.pkl', 'rb') as f:
+        test_triples = pickle.load(f)
+
+    triples_entities, triples_predicates = [], []
+
+    for triple in train_triples + valid_triples + test_triples:
+        triples_entities += [triple[0], triple[2]]
+        triples_predicates.append(triple[1])
 
 
+    triples_entities = sorted(list(set(triples_entities)))
+    triples_predicates = sorted(list(set(triples_predicates)))
+
+    # uritoid = {ent: i for i, ent in enumerate(['__na__', '__pad__'] + entities +  predicates)}
+    entoid = {pred: i for i, pred in enumerate(triples_entities)}
+    prtoid = {pred: i for i, pred in enumerate(triples_predicates)}
+
+    train = [[entoid[q[0]], prtoid[q[1]], entoid[q[2]]] for q in train_triples]
+    valid = [[entoid[q[0]], prtoid[q[1]], entoid[q[2]]] for q in valid_triples]
+    test = [[entoid[q[0]], prtoid[q[1]], entoid[q[2]]] for q in test_triples]
+
+    return {"train": train, "valid": valid, "test": test}
+
+
+if __name__ == "__main__":
+    quints = load_wd15k_quints()
+    triples = load_wd15k_triples()
