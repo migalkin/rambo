2bfe4f33e85ffffdd4b87cf143222b49a1b0e1bc
Michael Galkin <mikhgalkin@gmail.com>
2020-04-18 03:32:26 +0200

    super last try

--- gnn_layer.py
+++ gnn_layer.py
@@ -177,7 +177,7 @@ class CompQGCNConvLayer(MessagePassing):
                         subbatch_q_index = torch.tensor([k for k, x in enumerate(edges) if x in self.quals_index_in for _ in range(self.quals_index_in.eq(x).sum().item())], device=self.device)
                         #subbatch_in_quals = torch.tensor([k for k, x in enumerate(self.quals_index_in) if x in edges], device=self.device)
                         # subbatch_in_quals = torch.tensor([l.item() for k in edges if k in self.quals_index_in for l in torch.nonzero(self.quals_index_in == k)])
-                        subbatch_in_quals = torch.cat([torch.nonzero(self.quals_index_in == k).view(1,-1).squeeze(0) for k in edges if k in self.quals_index_in])
+                        subbatch_in_quals = torch.cat([torch.nonzero(self.quals_index_in == k) for k in edges]).squeeze(1)
                         subbatch_in_q_ents = self.in_index_qual_ent[subbatch_in_quals]
                         subbatch_in_q_rels = self.in_index_qual_rel[subbatch_in_quals]
                         # temp1 = in_edges_with_quals[..., None] == edges
