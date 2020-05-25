3955872533a13971cdaf15a76dc5d96ae6bd7f4d
Michael Galkin <mikhgalkin@gmail.com>
2020-05-25 16:24:57 +0200

    wikidata labels for predictions

--- stare_analysis.py
+++ stare_analysis.py
@@ -2,6 +2,21 @@ import torch
 from data_manager import DataManager
 from models_statements import CompGCN_Transformer
 import json
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+def get_label(item:str) -> str:
+    sparql.setQuery("""
+    SELECT ?itemLabel
+    WHERE
+    {{
+        wd:{0!s} rdfs:label ?itemLabel . FILTER (lang(?itemLabel)="en")
+    }}
+    """.format(item))
+    sparql.setReturnFormat(JSON)
+    sparql.addCustomHttpHeader("User-Agent", "stare")
+    results = sparql.query().convert()
+    return results['results']['bindings'][0]['itemLabel']['value']
 
 # that dict was not saved, so redefining the saved config here
 COMPGCNARGS = {
@@ -52,7 +67,7 @@ config = json.load(open("models/stare_s/config.json","r"))
 config['COMPGCNARGS'] = COMPGCNARGS
 data = DataManager.load(config=config)()
 try:
-    train_data, valid_data, test_data, n_entities, n_relations, _, _ = data.values()
+    train_data, valid_data, test_data, n_entities, n_relations, e2id, r2id = data.values()
 except ValueError:
     raise ValueError(f"Honey I broke the loader for {config['DATASET']}")
 
@@ -64,6 +79,8 @@ reci_valid = DataManager.add_reciprocals(valid_data, config)
 valid_data.extend(reci_valid)
 reci_test = DataManager.add_reciprocals(test_data, config)
 test_data.extend(reci_test)
+id2e = {v:k for k,v in e2id.items()}
+id2p = {v:k for k,v in r2id.items()}
 
 config['DEVICE'] = torch.device("cpu")
 
@@ -76,5 +93,14 @@ sub, rel, quals = data_p[:, 0], data_p[:, 1], data_p[:, 3:].contiguous()
 preds = model(sub, rel, quals)
 res = torch.argsort(preds, dim=1, descending=True)
 for i in range(res.shape[0]):
-    print(f"Correct: {data_p[i][2]}. Top 10 predictions: ",res[i][:10])
+    print(f"Triple: "
+          f"{get_label(id2e[int(sub[i])])}, "
+          f"{get_label(id2p[int(rel[i])])}, "
+          f"{get_label(id2e[int(data_p[i][2])])}. ")
+    if int(quals[i][0]) != 0:
+        print("Quals: ", [get_label(id2p[int(quals[i][k])]) if k % 2 == 0 else get_label(id2e[int(quals[i][k])]) for k in range(quals[i].shape[0]) if int(quals[i][k])!=0] )
+    else:
+        print("Quals: None")
+    print(f"Top 10 predictions: ", [get_label(id2e[int(k)]) for k in res[i][:10]])
+    print("-"*20)
 
