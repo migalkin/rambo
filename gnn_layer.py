24ffcaf7f325ba3f97558c68200f7561843a64de
Michael Galkin <mikhgalkin@gmail.com>
2020-07-03 01:00:52 +0200

    disentangled qual aggregate attention and general attention

--- gnn_layer.py
+++ gnn_layer.py
@@ -63,6 +63,11 @@ class CompQGCNConvLayer(MessagePassing):
                 'EMBEDDING_DIM'], "Current attn implementation requires those tto be identical"
             assert self.p['EMBEDDING_DIM'] % self.p['COMPGCNARGS']['ATTENTION_HEADS'] == 0, "should be divisible"
             self.att_qual = get_param((1, self.heads, 2 * self.attn_dim))
+            if not self.p['COMPGCNARGS']['ATTENTION']:
+                self.heads = self.p['COMPGCNARGS']['ATTENTION_HEADS']
+                self.attn_dim = self.out_channels // self.heads
+                self.negative_slope = self.p['COMPGCNARGS']['ATTENTION_SLOPE']
+                self.attn_drop = self.p['COMPGCNARGS']['ATTENTION_DROP']
 
         if self.p['COMPGCNARGS']['BIAS']: self.register_parameter('bias', Parameter(
             torch.zeros(out_channels)))
